e985a88e612becd67e04726cef0f11fcf16a539f
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-09-18 17:13:22 +0900

    1. main title

--- src/App.js
+++ src/App.js
@@ -19,15 +19,49 @@ const AppContainer = styled.div`
 const MainPic = styled.div`
   width: 100vw;
   height: 100vh;
+  position: relative;
   background-image: url(${mainPic});
   background-size: cover;
   background-position: center;
+  div:nth-child(1) {
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 40px;
+    font-weight: 800;
+    color: white;
+  }
+  div:nth-child(2) {
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 30px;
+    font-weight: 400;
+    color: white;
+  }
+  div:nth-child(3) {
+    position: absolute;
+    top: 38%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 50px;
+    font-weight: 800;
+    color: white;
+ 
 `;
 
 function App() {
   return (
     <AppContainer>
-      <MainPic></MainPic>
+      <MainPic>
+        <div>KHD 구매대행</div>
+        <div>"글로벌 쇼핑 경험을 높이는 전문 해외직구 파트너"</div>
+        <div>
+          <span>KHD 구매대행과 함께</span>
+        </div>
+      </MainPic>
       <WhyNeed></WhyNeed>
       <Banner></Banner>
       <Service></Service>
